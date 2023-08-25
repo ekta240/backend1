4041ab2b53e1592214043955b86a45720bcab159
ekta240 <ektasil01@gmail.com>
2023-08-25 09:07:42 +0545

    First comment

---
+++ model/orderModal.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+
+    orderNumber: {
+        type: String,
+        default: Math.floor(Math.random() * 100000000)
+    },
+
+    cart:[
+        {
+            productName:{
+                type:String,
+            },
+            productPrice:{
+                type: Number,
+            },
+            productCategory:{
+type:String,
+            },
+            productImage:{
+                type:String,
+            },
+            productQuantity:{
+                type:Number,
+            }
+        }
+    ],
+
+    totalAmount:{
+        type: Number
+    },
+    shippingAddress:{
+        type:String,
+    },
+
+    status:{
+        type:String,
+        default: "Pending"
+    },
+
+    orderedDate:{
+        type:Date,
+        default:Date.now
+    },
+    
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    }
+})
+
+module.exports = mongoose.model("OrderModal", orderSchema)
